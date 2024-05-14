4910abaf1c6244d20eda94e92cd6f38abba56211
38760 <musovicb@gmail.com>
2024-05-14 20:15:14 +0200

    put database information into .env. Made header and footer

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ import express from 'express';
 import { createClient } from '@supabase/supabase-js';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import headerFooter from './routes/headerFooter.js';
 
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config();
+
+// Set view engine to EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 // using morgan for logs
 app.use(morgan('combined'));
@@ -11,16 +24,19 @@ app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 const supabase = createClient(
-  'https://logojaerhofyvybmnxvm.supabase.co', // Project URL
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxvZ29qYWVyaG9meXZ5Ym1ueHZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU2MzYwMjksImV4cCI6MjAzMTIxMjAyOX0.zUYra9CmwYNeOmI9ih3uLOXMsntByiOvNReNyg6wXVc' // API Key (Anon Key)
+  String(process.env.SUPABASE_URL), // Project URL
+  String(process.env.SUPABASE_ANON_KEY) // API Key (Anon Key)
 );
+
+app.get('/', headerFooter);
 
 app.get('/user', async (req, res) => {
   const { data, error } = await supabase.from('user').select();
   res.send(data);
 });
-
 
 app.listen(3000, () => {
   console.log(`> Ready on http://localhost:3000`);
